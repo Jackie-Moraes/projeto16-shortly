02439947beb0dee12d6db597afffa3abf614bc81
Jackie-Moraes <patrickandradepatrick@hotmail.com>
2022-06-08 14:03:30 -0300

    heroku setup

--- database.js
+++ database.js
@@ -4,27 +4,13 @@ dotenv.config()
 
 const { Pool } = pg
 
-// const databaseConfig = {
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//         rejectUnauthorized: false,
-//     },
-// }
+const databaseConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+}
 
-// const connection = new Pool(databaseConfig)
-
-const user = process.env.PSQL_USER
-const password = process.env.PSQL_PASSWORD
-const host = "localhost"
-const port = 5432
-const database = "shortly"
-
-const connection = new Pool({
-    user,
-    password,
-    host,
-    port,
-    database,
-})
+const connection = new Pool(databaseConfig)
 
 export default connection
